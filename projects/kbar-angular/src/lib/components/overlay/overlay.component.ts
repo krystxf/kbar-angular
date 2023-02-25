87594c73d2cf5e973aa35182f7a2f5df3bec2586
Kryštof Krátký <krystof.kratky2003@gmail.com>
2023-02-25 16:35:09 +0100

    overlay customization

--- projects/kbar-angular/src/lib/components/overlay/overlay.component.ts
+++ projects/kbar-angular/src/lib/components/overlay/overlay.component.ts
@@ -1,16 +1,24 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { KbarAngularService } from '../../kbar-angular.service';
 
 @Component({
   selector: 'kbar-overlay',
   styleUrls: ['./overlay.component.css'],
   template: `
-    <div class="overlay" (click)="onClick($event)">
+    <div
+      [ngClass]="{ overlay: !unstyled }"
+      (click)="onClick($event)"
+      [style]="style"
+      [ngStyle]="ngStyle"
+    >
       <ng-content></ng-content>
     </div>
   `,
 })
 export class OverlayComponent {
+  @Input() unstyled: boolean = false;
+  @Input() style: any = {};
+  @Input() ngStyle: { [klass: string]: any } = {};
   @Output() handleClick: EventEmitter<MouseEvent> = new EventEmitter();
 
   constructor(private _kbarAngularService: KbarAngularService) {}
